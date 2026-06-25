9fd22be661fe6a4eecf28721c4e561ad30562c56
pedrocb02 <163739607+pedrocb02@users.noreply.github.com>
2026-06-25 15:28:59 -0300

    feat: funções addTransacao e calcularSaldo

--- GerenciadorFinanceiro.py
+++ GerenciadorFinanceiro.py
@@ -1,10 +1,22 @@
 import pandas as pd
 import numpy as np
 
-from Transacao import Transacao
+from Transacao import Transacao, TipoTransacao
 from MetaMensal import MetaMensal
 
 class GerenciadorFinanceiro:
     def __init__(self):
         self.lista_transacoes = []
         self.lista_metas = []
+
+    def addTransacao(self, transacao: Transacao):
+            self.lista_transacoes.append(transacao)
+    
+    def calcularSaldo(self):
+        saldo = 0
+        for i in self.lista_transacoes:
+            if(i.tipo == TipoTransacao.RECEITA):
+                saldo += i.valor
+            else:
+                saldo -= i.valor
+        return saldo
