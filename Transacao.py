5eb202753c4c852ebcee6923542c3ceec2f5c408
pedrocb02 <163739607+pedrocb02@users.noreply.github.com>
2026-06-25 15:08:03 -0300

    feat: adicionar tipo enum TipoTransacao

--- Transacao.py
+++ Transacao.py
@@ -1,11 +1,15 @@
-class Transacao:
+from enum import Enum
 
+class TipoTransacao(Enum):
+    RECEITA = "receita"
+    DESPESA = "despesa"
+
+class Transacao:
     #só a criação do construtor bem simples
     # categoria diz se é essencial, lazer,  alimentação
-    def __init__(self, data, descricao, valor, tipo, categoria):
+    def __init__(self, data, descricao, valor, tipo: TipoTransacao, categoria):
         self.data = data
         self.descricao = descricao
         self.valor = valor
         self.tipo = tipo
         self.categoria = categoria
-
