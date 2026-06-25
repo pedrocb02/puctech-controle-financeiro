bccf7dc1f302d8c438c42f3019a23307fc9f574a
pedrocb02 <163739607+pedrocb02@users.noreply.github.com>
2026-06-25 14:45:09 -0300

    feat: estrutura base do GerenciadorFinanceiro

--- GerenciadorFinanceiro.py
+++ GerenciadorFinanceiro.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
 
+from Transacao import Transacao
+from MetaMensal import MetaMensal
+
+class GerenciadorFinanceiro:
+    def __init__(self):
+        self.lista_transacoes = []
+        self.lista_metas = []
