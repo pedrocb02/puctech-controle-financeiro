99e2af525c6c8674c7b275bee1656cbf46a3dcfa
MatheusC307 <matheuscastelo222@gmail.com>
2026-06-26 20:41:29 -0300

    Feat: adicionei campo categoria nas transacoes

--- Transacao.py
+++ Transacao.py
@@ -5,11 +5,12 @@ class TipoTransacao(Enum):
     DESPESA = "despesa"
 
 class Transacao:
-    def __init__(self, data, descricao, valor, tipo: TipoTransacao):
+    def __init__(self, data, descricao, valor, tipo: TipoTransacao, categoria):
         self.data = data
         self.descricao = descricao
         self.valor = float(valor)
         self.tipo = tipo
+        self.categoria = categoria
 
     def __str__(self):
       return str(self.__dict__)
